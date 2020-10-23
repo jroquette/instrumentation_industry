d59ff129194b98a14a246fecc3faf24f0153c84d
Jose Henrique Roquette <roquettejh@gmail.com>
2020-10-23 13:54:31 -0400

    Change to postgresql db

--- instrumentation_industry/settings.py
+++ instrumentation_industry/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'client',
     'industry',
     'instrumentation',
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'instrumentation_industry.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangodb',
+        'USER': 'django',
+        'PASSWORD': 'django123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -103,6 +108,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
