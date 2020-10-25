0e95bcbcaead0579f0aa18ec375b48666392e173
Jose Henrique Roquette <roquettejh@gmail.com>
2020-10-25 15:58:38 -0400

    Change authentication response

---
+++ instrumentation_industry/authentication.py
@@ -0,0 +1,14 @@
+from rest_framework import authentication
+from rest_framework import exceptions
+
+
+class CustomAuthentication(authentication.BasicAuthentication):
+    """Custom Authentication"""
+
+    def authenticate(self, request):
+        """Override of authenticate methods"""
+        try:
+            response = super().authenticate(request)
+            return response
+        except:
+            raise exceptions.PermissionDenied("Credentials Invalid.")
